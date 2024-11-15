76d6e050b1d43f56a4e7ff893e73741d452af567
jmqian1009 <1830010557@qq.com>
2024-11-15 10:09:39 +0800

    every node is tested

--- test_tb.sv
+++ test_tb.sv
@@ -1,5 +1,5 @@
 module test_tb;
-parameter DW = 32+3;
+parameter DW = 32 + 3;
 parameter N  = 8;
 //
 logic          clk;
@@ -11,6 +11,9 @@ logic          i_ready [N-1:0];
 logic          o_valid [N-1:0];
 logic          o_ready [N-1:0];
 logic          start;
+logic          nxt_start;
+logic [$clog2(N)-1:0] src_idx;
+logic [$clog2(N)-1:0] dst_idx;
 //clk rst
 initial begin
     clk = 1'b0;
@@ -30,6 +33,41 @@ generate
         assign o_ready[i] = 1'b1;
     end
 endgenerate
+//src_idx
+always_ff@(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        src_idx <= '0;
+    end
+    else if(i_valid[src_idx] && i_ready[src_idx] && dst_idx == N - 1) begin
+        src_idx <= src_idx + 1'b1;
+    end
+end
+//dst_idx
+always_ff@(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        dst_idx <= '0;
+    end
+    else if(i_valid[src_idx] && i_ready[src_idx]) begin
+        dst_idx <= dst_idx + 1'b1;
+    end
+end
+//nxt_start
+always_ff@(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        nxt_start <= 1'b0;
+    end
+    else if(i_valid[src_idx] && i_ready[src_idx]) begin
+        if(src_idx == N - 1 && dst_idx == N - 1) begin
+            nxt_start <= 1'b0;
+        end
+        else begin
+            nxt_start <= 1'b1;
+        end
+    end
+    else begin
+        nxt_start <= 1'b0;
+    end
+end
 //drv input
 always_ff@(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
@@ -38,19 +76,32 @@ always_ff@(posedge clk or negedge rst_n) begin
             i_data[i]  <= DW'(0);
         end
     end
-    else if(start == 1'b1) begin
-        i_valid[5] <= 1'b1;
-        i_data[5]  <= {3'd6, 32'h1234};              //5--->6
+    else if(start == 1'b1 || nxt_start == 1'b1) begin
+        i_valid[src_idx] <= 1'b1;
+        i_data[src_idx]  <= {dst_idx, 32'(src_idx)};              //5--->6
+        $display("src node %d send data %h to dst node %d",src_idx, 32'(src_idx), dst_idx);
     end
-    else if(i_valid[5] && i_ready[5]) begin
-        i_valid[5] <= 1'b0;
-        i_data[5]  <= DW'(0);
+    else if(i_valid[src_idx] && i_ready[src_idx]) begin
+        i_valid[src_idx] <= 1'b0;
+        i_data[src_idx]  <= DW'(0);
+    end
+end
+//receive data and check
+always_ff@(posedge clk) begin
+    for(int i = 0; i < N; i++) begin
+        if(o_valid[i] && o_ready[i]) begin
+            $display("dst node %d receive data %h", i[$clog2(N)-1:0], o_data[i][31:0]);
+            if(o_data[i][32 +: $clog2(N)] != i[$clog2(N)-1:0]) begin
+                $display("test failed");
+                $finish;
+            end
+        end
     end
 end
 //start
 initial begin
     start = 1'b0;
-    #400
+    #200
     start = 1'b1;
     #10
     start = 1'b0;
@@ -63,7 +114,8 @@ initial begin
 end
 
 initial begin
-    #1000
+    #10000
+    $display("test pass");
     $finish;
 end
 //inst
